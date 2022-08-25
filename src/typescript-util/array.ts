08d728072be3a131264e24fec5a515fc967dcd96
paikwiki <paikwiki@gmail.com>
2022-08-25 23:25:19 +0900

    :sparkles: Add typescript-util: arrayFilter()

--- src/typescript-util/array.ts
+++ src/typescript-util/array.ts
@@ -2,4 +2,8 @@ type ArrayMap = <T, U>(arr: T[], exec: (item: T) => U) => U[];
 const arrayMap: ArrayMap = <T, U>(arr: T[], exec: (item: T) => U) =>
   arr.map(exec);
 
-export { arrayMap };
+type ArrayFilter = <T>(arr: T[], cond: (item: T) => boolean) => T[];
+const arrayFilter: ArrayFilter = <T>(arr: T[], cond: (item: T) => boolean) =>
+  arr.filter((item) => cond(item));
+
+export { arrayMap, arrayFilter };
