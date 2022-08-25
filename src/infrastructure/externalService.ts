7d5a4a6f6e8625c8663a82cf1579baa1eef3e846
paikwiki <paikwiki@gmail.com>
2022-08-25 23:34:43 +0900

    :fire: Remove exteranlService.ts(dummy data)

--- src/infrastructure/externalService.ts
+++
@@ -1,8 +0,0 @@
-const fetchBooks = async () =>
-  Promise.resolve([
-    { title: "White Fang", quantity: "two" } as const,
-    { title: "Moby Dick", quantity: "one" } as const,
-    { title: "Hitchhiker's guide to the galaxy", quantity: 42 } as const,
-  ]);
-
-export { fetchBooks };
