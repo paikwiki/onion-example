34ded6cd66c5c94bd7bc80b2f646fa8c3205862b
paikwiki <paikwiki@gmail.com>
2022-08-27 00:32:45 +0900

    :label: Refactor: param type of bookGenerator()

--- src/domain/bookGenerator.ts
+++ src/domain/bookGenerator.ts
@@ -1,7 +1,7 @@
 import { arrayMapper } from "../typescript-util/array";
 import { bookAdapter } from "./bookAdapter";
+import type { FetchedBook } from "./schema";
 
-const bookGenerator = (books: Parameters<typeof bookAdapter>[0][]) =>
-  arrayMapper(books, bookAdapter);
+const bookGenerator = (books: FetchedBook[]) => arrayMapper(books, bookAdapter);
 
 export { bookGenerator };
