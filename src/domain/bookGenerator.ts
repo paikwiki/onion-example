4ed89df0db9817a46a6c733bc022d07984d953ac
paikwiki <paikwiki@gmail.com>
2022-08-27 00:33:59 +0900

    :adhesive_bandage: Rename param of bookGenerator()

--- src/domain/bookGenerator.ts
+++ src/domain/bookGenerator.ts
@@ -2,6 +2,7 @@ import { arrayMapper } from "../typescript-util/array";
 import { bookAdapter } from "./bookAdapter";
 import type { FetchedBook } from "./schema";
 
-const bookGenerator = (books: FetchedBook[]) => arrayMapper(books, bookAdapter);
+const bookGenerator = (fetchedBooks: FetchedBook[]) =>
+  arrayMapper(fetchedBooks, bookAdapter);
 
 export { bookGenerator };
