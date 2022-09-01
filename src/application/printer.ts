ccf6a7a7f5df7d549efacb75f7be4e534ad5ba95
paikwiki <paikwiki@gmail.com>
2022-09-01 14:53:57 +0900

    :necktie: Change execution order: print after generating

--- src/application/printer.ts
+++ src/application/printer.ts
@@ -5,8 +5,12 @@ import type { PrintFunction } from "./runtimeInterfaces";
 
 type Printer = (param: { books: Book[]; printFunction: PrintFunction }) => void;
 const printer: Printer = ({ books, printFunction }) =>
-  arrayMapper(books, ({ title, quantity }) =>
-    printFunction(`[${stringBackPadder(`Qty.${quantity}`, 6)}] TITLE: ${title}`)
+  printFunction(
+    arrayMapper(
+      books,
+      ({ title, quantity }) =>
+        `[${stringBackPadder(`Qty.${quantity}`, 6)}] TITLE: ${title}`
+    ).join("\n")
   );
 
 export { printer };
